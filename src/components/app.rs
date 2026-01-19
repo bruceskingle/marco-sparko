d72209471324fa0a019f16f6a564b05ac729f7fd
Bruce Skingle <github@skingle.org>
2026-01-19 16:43:28 +0000

    icons

--- src/components/app.rs
+++ src/components/app.rs
@@ -34,7 +34,7 @@ pub enum Route {
 
 // We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
 // The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
-const FAVICON: Asset = asset!("/assets/favicon.ico");
+const FAVICON: Asset = asset!("/icons/icon.ico");
 // The asset macro also minifies some assets like CSS and JS to make bundled smaller
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 
