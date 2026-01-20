db9d8e2c8e099f1133e96e5cf361afe924d7928c
Bruce Skingle <github@skingle.org>
2026-01-20 15:07:35 +0000

    clean and tidy

--- src/components/app.rs
+++ src/components/app.rs
@@ -25,9 +25,9 @@ pub enum Route {
         #[route("/")]
         Home {},
         // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
-        // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
-        #[route("/blog/:module_id")]
-        // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
+        // In this case, id will match any integer like `/module/123` or `/module/-456`.
+        #[route("/module/:module_id")]
+        // Fields of the route variant will be passed to the component as props. In this case, the module component must accept
         // an `id` prop of type `i32`.
         Module { module_id: String },
 }
